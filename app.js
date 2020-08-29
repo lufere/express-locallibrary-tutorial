dc088e2bcf3df4ecbd5993111b855149fa1f2a87
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-08-28 23:44:42 -0700

    Created the library's models

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://lufere:<Y8CvaXLfuWGShnv>@cluster0.96lg6.mongodb.net/<local-library>?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser:true, useUnifiedTopology:true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
