e9832129e5c0ab2acdedcc25d1788f5d6aac7b0f
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-09-01 16:14:24 -0700

    Book instance controller and view were built

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,8 +1,14 @@
 var BookInstance = require('../models/bookinstance');
+const book = require('../models/book');
 
 // Display list of all BookInstances.
-exports.bookinstance_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance list');
+exports.bookinstance_list = function(req, res, next) {
+    BookInstance.find()
+        .populate('book')
+        .exec(function(err, list_bookinstances){
+            if (err) {return next(err)}
+            res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_bookinstances});
+        });
 };
 
 // Display detail page for a specific BookInstance.
